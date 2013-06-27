32a0f85fae324f7a454766dc1c47b6b17b834b9c
JHE <jan.herich@gmail.com>
2013-06-27 17:28:56 +0200

    add subproject definitions

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject itedge/service-hub "1.1.0-SNAPSHOT"
+  :plugins [[lein-sub "0.2.3"]]
+  :sub ["core"
+        "persistence-korma"
+        "persistence-datomic"
+        "http-ring"
+        "http-pedestal"])
