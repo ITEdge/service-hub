bdffb97bfa6cac5dcac5874fe1596f6d8afd6a6f
JHE <jan.herich@gmail.com>
2013-07-02 09:50:52 +0200

    Unit tests

--- common/test/itedge/service_hub/common/test/util.clj
+++ common/test/itedge/service_hub/common/test/util.clj
@@ -2,3 +2,5 @@
   (:use itedge.service-hub.common.util
         clojure.test))
 
+(deftest test-update-map-keys-values
+  (is (= (update-map-keys-values {1 {:a "t"} 2 {:b "t"}} (fn [k v] (assoc v :k k))) {1 {:k 1 :a "t"} 2 {:k 2 :b "t"}})))
