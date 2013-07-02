e8f1885cb562927b17e2a70d689bcf44c49cd038
JHE <jan.herich@gmail.com>
2013-07-02 10:00:37 +0200

    More unit tests

--- common/test/itedge/service_hub/common/test/util.clj
+++ common/test/itedge/service_hub/common/test/util.clj
@@ -4,3 +4,7 @@
 
 (deftest test-update-map-keys-values
   (is (= (update-map-keys-values {1 {:a "t"} 2 {:b "t"}} (fn [k v] (assoc v :k k))) {1 {:k 1 :a "t"} 2 {:k 2 :b "t"}})))
+
+(deftest test-update-if-in
+  (is (= (update-if-in {:a 1 :b 2} :c inc) {:a 1 :b 2}))
+  (is (= (update-if-in {:a 1 :b 2} :b inc) {:a 1 :b 3})))
