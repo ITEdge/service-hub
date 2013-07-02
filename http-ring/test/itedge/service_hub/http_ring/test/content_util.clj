62e0b985558f0796de8b5ed0846a0f21ad58c49e
JHE <jan.herich@gmail.com>
2013-07-02 18:39:40 +0200

    unit tests for http-ring sub-project

---
+++ http-ring/test/itedge/service_hub/http_ring/test/content_util.clj
@@ -0,0 +1,16 @@
+(ns itedge.service-hub.http-ring.test.content-util
+  (:use itedge.service-hub.http-ring.content-util
+        clojure.test))
+
+(deftest test-craft-json-response
+  (let [{headers :headers body :body} (craft-json-response [{:a 1 :b 2} "test" nil])]
+    (is (= body "[{\"a\":1,\"b\":2},\"test\",null]"))
+    (is (= (get headers "Content-Type") "application/json"))))
+
+(deftest test-read-json
+  (is (= (read-json "[{\"a\":1,\"b\":2},\"test\",null]") [{:a 1 :b 2} "test" nil])))
+
+(deftest test-craft-edn-response
+  (let [{headers :headers body :body} (craft-edn-response [{:a 1 :b 2} "test" nil])]
+    (is (= body "[{:a 1, :b 2} \"test\" nil]"))
+    (is (= (get headers "Content-Type") "application/edn"))))
---
+++ http-ring/test/itedge/service_hub/http_ring/test/routes_util.clj
@@ -0,0 +1,4 @@
+(ns itedge.service-hub.http-ring.test.routes-util
+  (:use itedge.service-hub.http-ring.routes-util
+        clojure.test))
+
