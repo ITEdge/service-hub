4cbb85a94d497e091d533f368e1e3173d924bef5
JHE <jan.herich@gmail.com>
2013-07-02 18:39:11 +0200

    memory handler implementation along with unit tests

---
+++ core/src/itedge/service_hub/core/handlers_memory.clj
@@ -0,0 +1,61 @@
+(ns itedge.service-hub.core.handlers-memory
+  (:use itedge.service-hub.core.handlers)
+  (:require [itedge.service-hub.core.util :as util]))
+
+(defn- index-data [data id-key index]
+  (reduce (fn [acc item]
+            (let [next-id (swap! index inc)]
+              (assoc acc next-id (assoc item id-key next-id)))) {} data))
+
+(defn- compare-fn [v expression-map]
+  (let [expression (if (map? expression-map) (first expression-map) (first {:value expression-map}))
+        function-key (key expression)
+        compare-value (val expression)
+        compare-function (function-key {:not not=
+										                    :not-in (fn [v c-v] (not (util/in? c-v v)))
+                                        :in (fn [v c-v] (util/in? c-v v))
+										                    :gt > 
+										                    :lt <
+										                    :gteq >=
+										                    :lteq <=
+                                        :value =})]
+    (compare-function v compare-value)))
+
+(defn- filter-fn [criteria]
+	(fn [map-entry]
+    (if criteria
+      (let [v (val map-entry)]
+        (every? (fn [e] (compare-fn ((key e) v) (val e))) criteria))
+      true)))
+
+(defn create-memory-handler [data id-key]
+  (let [index (atom 0)
+        entity-map (atom (index-data data id-key index))]
+    (reify PEntityHandler
+      (handle-find-entity [_ id]
+        (get @entity-map id))
+		  (handle-exist-entity [_ id]
+		    (if (get @entity-map id)
+		      true
+		      false))
+		  (handle-delete-entity [_ id]
+		    (when (get @entity-map id)
+          (swap! entity-map dissoc id)
+          id))
+		  (handle-update-entity [_ attributes]
+		    (when-let [id (id-key attributes)]
+          (when-let [old (get @entity-map id)]
+            (let [updated (merge old attributes)]
+              (swap! entity-map assoc id updated)
+              updated)))) 
+		  (handle-add-entity [_ attributes]
+        (let [next-id (swap! index inc)
+              attributes (assoc attributes id-key next-id)]
+          (swap! entity-map assoc next-id attributes)
+          attributes))
+		  (handle-list-entities [_ criteria sort-attrs from to]
+		    (into [] (map second (filter (filter-fn criteria) @entity-map)))) ; sorting and paging not implemented
+		  (handle-count-entities [_ criteria]
+		    (count (filter (filter-fn criteria) @entity-map)))
+		  (handle-get-unique-identifier [_]
+		    id-key))))
---
+++ core/test/itedge/service_hub/core/test/handlers_memory.clj
@@ -0,0 +1,25 @@
+(ns itedge.service-hub.core.test.handlers-memory
+  (:use itedge.service-hub.core.handlers-memory
+        itedge.service-hub.core.handlers
+        clojure.test))
+
+(def test-handler (create-memory-handler [{:name "test-entity-one" :thing 2}
+                                          {:name "test-entity-two" :thing 7}] :id))
+
+(handle-add-entity test-handler {:name "test-entity-three" :thing 11})
+(handle-add-entity test-handler {:name "test-entity-four" :thing 7})
+
+(handle-update-entity test-handler {:id 1 :thing 3})
+
+(deftest test-memory-handler
+  (let [entity-one (handle-find-entity test-handler 1)
+        entity-two (handle-find-entity test-handler 2)
+        entity-three (handle-find-entity test-handler 3)
+        q-result-one (handle-list-entities test-handler {:thing 7} nil nil nil)
+        q-result-two (handle-list-entities test-handler {:thing 7 :name "test-entity-two"} nil nil nil)]
+    (is (= (handle-count-entities test-handler nil) 4))
+    (is (= entity-one {:id 1 :name "test-entity-one" :thing 3}))
+    (is (= entity-two {:id 2 :name "test-entity-two" :thing 7}))
+    (is (= entity-three {:id 3 :name "test-entity-three" :thing 11}))
+    (is (= q-result-one [{:id 4 :name "test-entity-four" :thing 7} {:id 2 :name "test-entity-two" :thing 7}]))
+    (is (= q-result-two [{:id 2 :name "test-entity-two" :thing 7}]))))
