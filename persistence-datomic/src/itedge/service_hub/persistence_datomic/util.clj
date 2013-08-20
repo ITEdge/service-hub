a4ae659ee1d98f1b0e016a1d92763ec64a6456a3
JHE <jan.herich@gmail.com>
2013-08-20 15:02:15 +0200

    changes to support datomic

---
+++ persistence-datomic/src/itedge/service_hub/persistence_datomic/util.clj
@@ -0,0 +1,81 @@
+(ns itedge.service-hub.persistence-datomic.util
+  (:require [datomic.api :as d :refer [q entity touch transact]]
+            [clojure.set :as set]
+            [itedge.service-hub.core.util :as util]))
+
+(defn- extract-single [result]
+  (first (first result)))
+
+(defn exist-entity? 
+  "Determines if entity with given id exist in database"
+  [db id]
+  (if-let [e (extract-single (q '[:find ?eid :in $ ?eid :where [?eid]] db id))]
+    true
+    false))
+
+(defn get-entity
+  "Get entity with specified id, if no such entity exist, returns nil"
+  [db id]
+  (when (exist-entity? db id)
+    (touch (entity db id))))
+
+(defn- not-compare [v c-v]
+  (if (coll? v) (not (util/in? v c-v)) (not= v c-v)))
+
+(defn- not-in-compare [v c-v]
+  (not (util/in? c-v v)))
+
+(defn- in-compare [v c-v]
+  (util/in? c-v v))
+
+(defn- compare [v c-v]
+  (if (coll? v) (util/in? v c-v) (= v c-v)))
+
+(defn- get-function-expression [expression-map item-symbol]
+  (let [expression (if (map? expression-map) (first expression-map) (first {:value expression-map}))
+        expression-key (key expression)
+        compare-value (val expression)
+        func-body (-> '()
+                    (conj compare-value)
+                    (conj item-symbol))]
+	  (expression-key {:not [(conj func-body not-compare)]
+                     :not-in [(conj func-body not-in-compare)]
+	                   :in [(conj func-body in-compare)]
+	                   :gt [(conj func-body >)] 
+	                   :lt [(conj func-body <)]
+	                   :gteq [(conj func-body >=)]
+	                   :lteq [(conj func-body <=)]
+	                   :value [(conj func-body compare)]})))
+
+(defn- add-criteria [query entity-symbol criteria]
+  (reduce (fn [acc item]
+            (let [item-symbol (gensym "?item")]
+              (-> query 
+                (conj [entity-symbol (key item) item-symbol]) 
+                (conj (get-function-expression (val item) item-symbol))))) query criteria))
+
+(defn- add-fieldset [query entity-symbol fieldset]
+  (reduce (fn [acc item] (conj query [entity-symbol item])) query fieldset))
+
+(defn- unrestricted-fields [fieldset criteria]
+  (set/difference fieldset (set (keys criteria))))
+
+(defn count-entities
+  "Count entities with given fieldset in specified db"
+  [db fieldset criteria]
+  (let [query (-> '[:find (count ?e) :where]
+                (add-fieldset '?e (unrestricted-fields fieldset criteria))
+                (add-criteria '? criteria))]
+    (extract-single (q query db))))
+
+(defn list-entities
+  "List entities with given fieldset, criteria, sorting and paging in specified db"
+  [db fieldset criteria sort-attrs from to]
+  (let [entity-symbol '?e
+        query (-> [:find entity-symbol :where]
+                (add-fieldset entity-symbol (unrestricted-fields fieldset criteria))
+                (add-criteria entity-symbol criteria))
+        entity-ids (q query db)]
+    (map (fn [r] (touch (entity db (first r)))) entity-ids)))
+
+
---
+++ persistence-datomic/test/itedge/service_hub/persistence_datomic/test/util.clj
@@ -0,0 +1,69 @@
+(ns itedge.service-hub.persistence-datomic.test.util
+  (:require [datomic.api :as d :refer [q db entity create-database connect transact shutdown]]
+            [itedge.service-hub.persistence-datomic.util :refer :all]
+            [clojure.test :refer :all]))
+
+(def uri "datomic:mem://test")
+(create-database uri)
+(def conn (connect uri))
+(def schema-tx [
+{:db/id #db/id[:db.part/db]
+  :db/ident :item/name
+  :db/unique :db.unique/value
+  :db/valueType :db.type/string
+  :db/cardinality :db.cardinality/one
+  :db/fulltext true
+  :db/doc "A items's name"
+  :db.install/_attribute :db.part/db}
+
+ {:db/id #db/id[:db.part/db]
+  :db/ident :item/count
+  :db/valueType :db.type/long
+  :db/cardinality :db.cardinality/one
+  :db/doc "A item's count"
+  :db.install/_attribute :db.part/db}
+ 
+ {:db/id #db/id[:db.part/db]
+  :db/ident :item/price
+  :db/valueType :db.type/double
+  :db/cardinality :db.cardinality/one
+  :db/doc "A item's price"
+  :db.install/_attribute :db.part/db}
+ 
+ {:db/id #db/id [:db.part/db]
+  :db/ident :item/linked
+  :db/valueType :db.type/ref
+  :db/cardinality :db.cardinality/one
+  :db/doc "Linked articles"
+  :db.install/_attribute :db.part/db}
+])
+@(d/transact conn schema-tx)
+(def data-tx [
+{:db/id #db/id[:db.part/user -1] :item/name "item 1" :item/count 7 :item/price 20.3}
+{:db/id #db/id[:db.part/user -2] :item/name "item 2" :item/count 3 :item/price 11.9 :item/linked #db/id[:db.part/user -1]}
+{:db/id #db/id[:db.part/user -3] :item/name "item 3" :item/count 9 :item/price 17.8 :item/linked #db/id[:db.part/user -2]}
+])
+@(d/transact conn data-tx)
+
+(deftest test-exist-entity?
+  (let [id (first (first (q '[:find ?e :where [?e :item/name "item 1"]] (db conn))))]
+    (is (= (exist-entity? (db conn) id) true))
+    (is (= (exist-entity? (db conn) -1) false))))
+
+(deftest test-get-entity
+  (let [id (first (first (q '[:find ?e :where [?e :item/name "item 1"]] (db conn))))]
+    (is (= (:item/name (get-entity (db conn) id)) "item 1"))
+    (is (= (:db/id (get-entity (db conn) id)) id))
+    (is (= (get-entity (db conn) -1) nil))))
+
+(deftest test-count-entities
+  (is (= (count-entities (db conn) #{:item/name :item/count :item/price} nil) 3)))
+
+(deftest test-list-entities
+  (is (= (list-entities (db conn) #{:item/name :item/count :item/price} {:item/name "item 2"} nil nil nil) 
+         [{:item/name "item 1" :item/count 7 :item/price 20.3}])))
+
+(shutdown false)
+
+
+
