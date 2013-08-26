25516709c50ca022ff46b2c05c65b4ae3ba923a7
jhe <jan.herich@gmail.com>
2013-08-27 01:02:06 +0200

    compiler type hints in date-time-util

--- core/src/itedge/service_hub/core/date_time_util.clj
+++ core/src/itedge/service_hub/core/date_time_util.clj
@@ -4,40 +4,40 @@
            org.joda.time.format.ISODateTimeFormat
            org.joda.time.Duration))
 
-(defn timestamp-to-datetime
+(defn ^DateTime timestamp-to-datetime
   "Converts java.sql.Timestamp object to org.joda.time.DateTime object"
-  [Timestamp]
-  (DateTime. (.getTime Timestamp)))
+  [^Timestamp timestamp]
+  (DateTime. (.getTime timestamp)))
 
-(defn datetime-to-timestamp
+(defn ^Timestamp datetime-to-timestamp
   "Converts org.joda.time.DateTime object to java.sql.Timestamp object"
-  [DateTime]
-  (Timestamp. (.getMillis DateTime)))
+  [^DateTime datetime]
+  (Timestamp. (.getMillis datetime)))
 
 (defn datetime-to-iso-8061
   "Converts org.joda.time.DateTime object to ISO-8061 formatted string"
-  [DateTime]
-  (.print (ISODateTimeFormat/dateTime) DateTime))
+  [^DateTime datetime]
+  (.print (ISODateTimeFormat/dateTime) datetime))
 
 (defn get-duration-in-minutes
   "Gets duration in minutes between start and end times"
   [start end]
   (.getStandardMinutes (Duration. start end)))
 
-(defn iso-8061-to-datetime
+(defn ^DateTime iso-8061-to-datetime
   "Converts ISO-8061 formatted string to org.joda.time.DateTime object"
-  [String]
-  (DateTime. String))
+  [^String string]
+  (DateTime. string))
 
 (defn iso-8061?
   "Checks if string is compatible with ISO-8061 format"
   [iso-string]
   (try
-    (do (DateTime. iso-string)
+    (do ^DateTime (DateTime. iso-string)
       true)
     (catch IllegalArgumentException e false)))
 
-(defn get-current-date
+(defn ^DateTime get-current-date
   "Gets current time"
   []
   (DateTime.))
