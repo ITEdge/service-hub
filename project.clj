a1d4167ece038c406d141eb839090aa11facfc04
jhe <jan.herich@gmail.com>
2013-08-28 14:01:19 +0200

    upgrade lein-sub version to 0.2.4

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
 (defproject itedge/service-hub "1.2.1"
-  :plugins [[lein-sub "0.2.3"]]
+  :plugins [[lein-sub "0.2.4"]]
   :sub ["core"
         "common"
         "persistence-korma"
         "persistence-datomic" 
-        "http-ring"
-        ;"http-pedestal"
-        ])
+        "http-ring" ])
