5703034ca47f8b464fdde99dc2c8d6ed3f244994
jhe <jan.herich@gmail.com>
2013-09-06 20:56:16 +0200

    Add PEntityHistoryHandler

--- core/src/itedge/service_hub/core/handlers.clj
+++ core/src/itedge/service_hub/core/handlers.clj
@@ -13,8 +13,16 @@
   (handle-add-entity [this attributes] 
     "Persist new entity")
   (handle-list-entities [this criteria sort-attrs from to] 
-    "List all entities, optionally with criteria, optionaly sort them, and restrict their range")
+    "List all entities, optionaly with criteria, optionaly sort them and restrict their range")
   (handle-count-entities [this criteria] 
-    "Count all entities, optionally with criteria")
+    "Count all entities, optionaly with criteria")
   (handle-get-unique-identifier [this]
     "Returns unique identifier for entity"))
+
+(defprotocol PEntityHistoryHandler
+  "A protocol for history enabled entity handlers"
+  (handle-list-entity-history [this id criteria sort-attrs from to]
+    "List all history states (snapshots - map of all entity atributes along with snapshot time and transaction id) of entity values, 
+     optionaly with criteria, optionaly sort them and restrict their range")
+  (handle-find-entity-history [this entity-id history-id]
+    "Find one history state of the entity by primary id of the entity and history id (could be transaction id or time)"))
