def0f4d3c556112eb02ed527bfd36e5c87027fd3
jhe <jan.herich@gmail.com>
2013-09-06 12:14:45 +0200

    Begin 1.3.2-SNAPSHOT development

--- persistence-korma/project.clj
+++ persistence-korma/project.clj
@@ -1,9 +1,9 @@
-(defproject itedge/service-hub.persistence-korma "1.3.1"
+(defproject itedge/service-hub.persistence-korma "1.3.2-SNAPSHOT"
   :description "Service-Hub korma persistence layer (SQL abstraction layer)"
   :min-lein-version "2.0.0"
   :url "https://github.com/ITEdge/service-hub"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [itedge/service-hub.core "1.3.1"]
+                 [itedge/service-hub.core "1.3.2-SNAPSHOT"]
                  [korma-enhanced "0.3.1"]])
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject itedge/service-hub "1.3.1"
+(defproject itedge/service-hub "1.3.2-SNAPSHOT"
   :plugins [[lein-sub "0.2.4"]]
   :sub ["core"
         "common"
