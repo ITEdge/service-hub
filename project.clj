6d520b4ccec322fe789f957cf7bcd3ff1fd92868
jhe <jan.herich@gmail.com>
2015-10-30 13:04:34 +0100

    Add general project info

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
 (defproject itedge/service-hub "1.3.3"
+  :description "Rapid development framework"
+  :url "https://github.com/ITEdge/service-hub"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :plugins [[lein-sub "0.2.4"]]
   :sub ["core"
         "common"
         "persistence-korma"
-        "persistence-datomic" 
+        "persistence-datomic"
         "http-ring" ])
